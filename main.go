341ddbf3660d9f96691213a5e43b276649e243c4
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-10 06:17:31 +0300

    swagger files created

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"net/http"
 
+	"github.com/aabdullahgungor/go-gin-swagger/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // album represents data about a record album.
@@ -22,11 +25,25 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-// getAlbums responds with the list of all albums as JSON.
+// GetAlbums             godoc
+//	@Summary		Get albums array
+//	@Description	Responds with the list of all albums as JSON.
+//	@Tags			albums
+//	@Produce		json
+//	@Success		200	{array}	album
+//	@Router			/albums [get]
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// postAlbums            godoc
+//	@Summary		Store a new album
+//	@Description	Takes a album JSON and store in DB. Return saved JSON.
+//	@Tags			albums
+//	@Produce		json
+//	@Param			album	body		album	true	"Album JSON"
+//	@Success		200		{object}	album
+//	@Router			/albums [post]
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -38,6 +55,14 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+// getAlbumByID                godoc
+//	@Summary		Get single album by id
+//	@Description	Returns the album whose id value matches the id.
+//	@Tags			albums
+//	@Produce		json
+//	@Param			id	path		string	true	"search album by id"
+//	@Success		200	{object}	album
+//	@Router			/albums/{id} [get]
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
@@ -50,12 +75,40 @@ func getAlbumByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
+
+//	@securityDefinitions.basic	BasicAuth
+
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = ""
+	docs.SwaggerInfo.BasePath = "/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run("localhost:8080")
 
